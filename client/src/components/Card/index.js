38104e5a226473d781bfe102527942839e1e4149
anniemcfarland7 <anniemcfarland@aol.com>
2021-04-13 21:17:15 -0500

    begin card

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => (
+  <div className="card">
+    <img className="card-img-top" src="..." alt="brewery" />
+    <div className="card-body">
+      <h5 className="card-title">
+        {/* <a href="">
+          brewery name here
+        </a> */}
+        SAMPLE NAME
+      </h5>
+      <i class="far fa-heart" />
+    </div>
+  </div>
+);
+
+export default Card;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import Card from "../components/Card";
 
 function Home() {
   return (
-<main>
-  <SearchBar/>
-</main>
-  )
+    <main>
+      <SearchBar />
+      <div className="container">
+        <div className="row row-cols-md-3 mx-auto mt-5">
+            <Card
+              key=""
+              image=""
+              name=""
+              breweryURL=""
+            />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
+
+// NEED TO TIE IN OPENBREWERY API
